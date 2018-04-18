7091a189d660759c760907371af383016a5773a7
Min <jiangmin2015@ia.ac.cn>
2018-04-18 20:28:17 +0800

    Update AFM_NFM_Run.py

--- AFM_NFM_Run.py
+++ AFM_NFM_Run.py
@@ -48,11 +48,3 @@ model.train(data.Train_data, data.Validation_data, data.Test_data)
 best_epoch  = np.argmin(model.valid_loss)
 print ("Best Iter(validation)= %d\t train = %.4f, valid = %.4f, test = %.4f [%.1f s]" 
        %(best_epoch+1, model.train_loss[best_epoch], model.valid_loss[best_epoch], model.test_loss[best_epoch], time()-t1))
-
-
-# train:0.1114；valid：0.3201；test:0.3253 vaild-train = 0.2087 keep_prob = [0.7 0.8] Lamda_attention = 15.0 Epoch = 100 Best = 90 Optimizer = 'AdagradOptimizer' Lr = 0.05
-# train:0.1130；valid：0.3182；test:0.3237 vaild-train = 0.2052 keep_prob = [0.7 0.8] Lamda_attention = 15.0 Epoch = 100 Best = 73 Optimizer = 'AdamOptimizer' Lr = 0.001
-# train:0.1086；valid：0.3205；test:0.3281 vaild-train = 0.2195 keep_prob = [0.7 0.8] Lamda_attention = 15.0 Epoch = 100 Best = 98 Optimizer = 'AdamOptimizer' Lr = 0.0005
-# train:0.0760；valid：0.3489；test:0.3508 vaild-train = 0. keep_prob = [1 1] Lamda_attention = 15.0 Epoch = 100 Best = 98 Optimizer = 'AdamOptimizer' Lr = 0.001 Lamda_em = 0 Lamda_layers = 0 严重过拟合
-# train:0.5504；valid：0.5632；test:0.5677 vaild-train = 0. keep_prob = [1 1] Lamda_attention = 15.0 Epoch = 100 Best = 98 Optimizer = 'AdamOptimizer' Lr = 0.001 Lamda_em = 1 Lamda_layers = 1 严重欠拟合
-# train:0.0946；valid：0.3201；test:0.3256 vaild-train = 0.2052 keep_prob = [0.8 0.8] Lamda_attention = 15.0 Epoch = 100 Best = 100 Optimizer = 'AdamOptimizer' Lr = 0.001
